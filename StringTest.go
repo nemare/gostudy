17944d008a17029ba6e27659b180d59e0eebfbf6
piaozhikang <piaozhikang@didiglobal.com>
2020-10-25 16:49:30 +0800

    add

--- StringTest.go
+++ StringTest.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -13,4 +15,23 @@ func main() {
 
 	var b []string
 	fmt.Println(len(b))
+
+	var bigint int64
+	bigint = 144115188075865872
+	fmt.Println(strconv.FormatInt(bigint, 10))
+
+	//ab := make(map[string]interface{})
+	var ab map[string]interface{}
+	ab["resp"] = 12
+	c, ok := ab["resp"].(string)
+	if ok {
+		fmt.Println(c)
+	} else {
+		fmt.Println(ok)
+	}
+
+	fmt.Println(time.Unix(0, 1603294383368098192))
+
+	var num []int
+	fmt.Println(len(num))
 }
