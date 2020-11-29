1bdc5c49dbbf3b6bacdb4fe8ba80853cb05721fb
piaozhikang <piaozhikang@didiglobal.com>
2020-11-29 10:37:47 +0800

    add

---
+++ rangeTest.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Adad struct {
+	s string
+}
+
+func main() {
+
+	a1 := Adad{s: "abc"}
+	a2 := Adad{s: "abc"}
+	if reflect.DeepEqual(a1, a2) {
+		fmt.Println(a1, "==", a2)
+	}
+
+	b1 := []int{1, 2}
+	b2 := []int{1, 2}
+	if reflect.DeepEqual(b1, b2) {
+		fmt.Println(b1, "==", b2)
+	}
+
+	c1 := map[string]int{"a": 1, "b": 2}
+	c2 := map[string]int{"a": 1, "b": 2}
+	if reflect.DeepEqual(c1, c2) {
+		fmt.Println(c1, "==", c2)
+	}
+}
