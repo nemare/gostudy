f55397aa0f206e6262cbd1b746ac148762004198
piaozhikang <piaozhikang@didiglobal.com>
2021-02-08 11:32:37 +0800

    增加

---
+++ client.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/rpc"
+)
+
+func main() {
+
+	client, err := rpc.Dial("tcp", ":1234")
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+
+	var reply string
+	err = client.Call("HelloService.Hello", "world", &reply)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(reply)
+
+}
